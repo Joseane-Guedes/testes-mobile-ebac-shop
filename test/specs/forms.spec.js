fdecc7be10f559f8970105546ae5777cc6c984b1
JOSEANE GUEDES <joseane_23@hotmail.com>
2023-09-12 16:19:59 -0300

    Add exercicio modulo 16 form

---
+++ test/specs/forms.spec.js
@@ -0,0 +1,12 @@
+describe('Access forms', () => {
+    it('Access forms', async () => {
+
+        await driver.launchApp()
+        await $('//android.widget.Button[@content-desc="Forms"]/android.widget.TextView[2]').click();
+        await $('~text-input').setValue('test');
+
+        expect(await $('~input-text-result').getText()).toBe('test');
+
+        await $('~Dropdown').click();
+    });
+});
